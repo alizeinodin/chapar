291008ccb69058660262f9a6a9576b629e506e60
Ali Zeinodin <alizeinodin79@gmail.com>
2023-06-18 20:15:42 +0330

    AuthController add

---
+++ src/app/Http/Controllers/v1/Auth/AuthController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers\v1\Auth;
+
+use App\Http\Controllers\Controller;
+
+class AuthController extends Controller
+{
+    public function register()
+    {
+        # TODO implement registration
+    }
+
+    public function login()
+    {
+        # TODO implement login method
+    }
+}
