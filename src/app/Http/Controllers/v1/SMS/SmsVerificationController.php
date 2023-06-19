001188f307ba0236cc9e2f63c0779fb64fa7472b
Ali Zeinodin <alizeinodin79@gmail.com>
2023-06-19 17:50:47 +0330

    SMSVerificationController add for send sms and verify phone number of user

---
+++ src/app/Http/Controllers/v1/SMS/SmsVerificationController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers\v1\SMS;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class SmsVerificationController extends Controller
+{
+    public function send()
+    {
+        # TODO implement send sms verification for phone number
+    }
+
+    public function verify()
+    {
+        # TODO implement verify phone number of user
+    }
+}
