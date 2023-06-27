c86ccb0982348066465bb7a24edb7e32d0829705
Ali Zeinodin <alizeinodin79@gmail.com>
2023-06-27 13:02:28 +0330

    register method develop, many TODO's add for this method

--- src/app/Http/Controllers/v1/Auth/AuthController.php
+++ src/app/Http/Controllers/v1/Auth/AuthController.php
@@ -3,12 +3,39 @@
 namespace App\Http\Controllers\v1\Auth;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\Auth\RegisterRequest;
+use App\Models\User;
+use Symfony\Component\HttpFoundation\Response;
 
 class AuthController extends Controller
 {
-    public function register()
+    public function register(RegisterRequest $request): \Illuminate\Http\JsonResponse
     {
-        # TODO implement registration
+        $validatedData = $request->validated();
+
+        # TODO middleware checking for check duplicate phone number
+        # TODO add middleware for check phone number is validated or not
+
+        # TODO add repository pattern
+
+        # TODO add error handling for eloquent
+        $user = User::create([
+            'firstname' => $validatedData['firstname'],
+            'lastname' => $validatedData['lastname'] ?? null,
+            'phone' => $validatedData['phone'],
+            'username' => $validatedData['username'] ?? null,
+        ]);
+
+        $token = $user->createToken('access_token')->accessToken;
+
+        # TODO add global response for error and success
+        $response = [
+            'user' => $user,
+            'accessToken' => $token,
+            'message' => $user->firstname . _(' عزیز خوش اومدی :)'),
+        ];
+
+        return response()->json($response, Response::HTTP_CREATED);
     }
 
     public function login()
