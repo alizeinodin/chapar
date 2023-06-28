3cb2d43d40e8f61edb68824a205df0b361952164
Ali Zeinodin <alizeinodin79@gmail.com>
2023-06-28 15:09:35 +0330

    logout method add as todo

--- src/app/Http/Controllers/v1/Auth/AuthController.php
+++ src/app/Http/Controllers/v1/Auth/AuthController.php
@@ -41,4 +41,9 @@ class AuthController extends Controller
     {
         # TODO implement login method
     }
+
+    public function logout()
+    {
+        # TODO implement logout method
+    }
 }
