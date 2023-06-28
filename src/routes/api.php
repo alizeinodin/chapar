e563a79b49c0bc179859389000d005d45323912a
Ali Zeinodin <alizeinodin79@gmail.com>
2023-06-28 17:31:00 +0330

    auth api's add in api route's

--- src/routes/api.php
+++ src/routes/api.php
@@ -1,8 +1,5 @@
 <?php
 
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Route;
-
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -15,7 +12,4 @@ use Illuminate\Support\Facades\Route;
 */
 
 require_once 'API/SMSVerification.php';
-
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+require_once 'API/Auth.php';
