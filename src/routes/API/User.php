fe3833a6dc0950445019d59c51cbe79352f084a4
Ali Zeinodin <alizeinodin79@gmail.com>
2023-07-02 22:17:39 +0330

    user route add

---
+++ src/routes/API/User.php
@@ -0,0 +1,13 @@
+<?php
+
+use App\Http\Controllers\v1\User\UserController;
+use Illuminate\Support\Facades\Route;
+
+Route::controller(UserController::class)->group(function () {
+    Route::prefix('/user')->group(function () {
+        Route::name('user.')->group(function () {
+            Route::get('/get', 'getUser')
+                ->name('get');
+        });
+    });
+});
