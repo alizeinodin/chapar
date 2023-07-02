476e0579a619899f1a09fa83f54f169afb66e850
Ali Zeinodin <alizeinodin79@gmail.com>
2023-07-02 22:20:14 +0330

    SearchRequest add

---
+++ src/app/Http/Requests/Search/SearchRequest.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Requests\Search;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class SearchRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules(): array
+    {
+        return [
+            'content' => 'required|string',
+            'type' => 'in:username,name,email',
+        ];
+    }
+}
