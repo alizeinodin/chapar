dd77867a4f456caeac366efab09a6d755394e57c
Ali Zeinodin <alizeinodin79@gmail.com>
2023-07-04 17:20:16 +0330

    messages table add

---
+++ src/database/migrations/2023_07_04_130001_create_messages_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('messages', function (Blueprint $table) {
+            $table->id();
+
+            $table->foreignId('private_chat_id')
+                ->constrained()
+                ->onDelete('cascade')
+                ->onUpdate('cascade');
+
+            $table->foreignId('sender_id')
+                ->constrained('users', 'id')
+                ->onDelete('cascade')
+                ->onUpdate('cascade');
+
+            $table->foreignId('receiver_id')
+                ->constrained('users', 'id')
+                ->onDelete('cascade')
+                ->onUpdate('cascade');
+
+            $table->json('content');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('messages');
+    }
+};
