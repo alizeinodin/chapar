861ec4b2438a8010d35081d7a2b30b15d4bc7acd
Ali Zeinodin <alizeinodin79@gmail.com>
2023-07-06 20:30:30 +0330

    is_delete column add as boolean

--- src/database/migrations/2023_07_04_130001_create_messages_table.php
+++ src/database/migrations/2023_07_04_130001_create_messages_table.php
@@ -30,6 +30,8 @@ return new class extends Migration {
 
             $table->json('content');
 
+            $table->boolean('is_delete')->default(false);
+
             $table->timestamps();
         });
     }
