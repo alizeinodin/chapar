4c871b4400b5a882802b15b54201974e690d6a0a
Ali Zeinodin <alizeinodin79@gmail.com>
2023-07-06 20:31:14 +0330

    StoreMessageRequest add

---
+++ src/app/Http/Requests/Message/StoreMessageRequest.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Requests\Message;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreMessageRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules(): array
+    {
+        return [
+            'chat_id' => 'exists:private_chats,id',
+            'receiver_id' => 'required|exists:users,id',
+            'content' => 'required|json'
+        ];
+    }
+}
