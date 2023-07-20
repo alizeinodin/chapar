b5450e8040c5edbfa28ab31c7fab1501476e2c5e
alizeinodin <alizeinodin79@gmail.com>
2023-07-20 23:12:55 +0330

    TODO delete. this TODO develop

--- src/routes/API/Auth.php
+++ src/routes/API/Auth.php
@@ -4,8 +4,6 @@
 use App\Http\Controllers\v1\Auth\AuthController;
 use Illuminate\Support\Facades\Route;
 
-# TODO add middleware for time limitation for send sms
-
 Route::controller(AuthController::class)->group(function () {
     Route::prefix('/auth')->group(function () {
         Route::name('auth.')->group(function () {
