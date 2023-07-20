486f731840e8231b95c210d81cc341c68e6920c4
alizeinodin <alizeinodin79@gmail.com>
2023-07-20 19:44:11 +0330

    sms.limitation middleware add for send route api of SMSVerification.php

--- src/routes/API/SMSVerification.php
+++ src/routes/API/SMSVerification.php
@@ -3,14 +3,13 @@
 use App\Http\Controllers\v1\SMS\SmsVerificationController;
 use Illuminate\Support\Facades\Route;
 
-# TODO add middleware for time limitation for send sms
-
 Route::controller(SmsVerificationController::class)->group(function () {
     Route::prefix('/sms_verification')->group(function () {
         Route::name('sms_verification.')->group(function () {
 
             Route::post('/', 'send')
-                ->name('send');
+                ->name('send')
+                ->middleware('sms.limitation');
 
             Route::post('/verify', 'verify')
                 ->name('verify');
