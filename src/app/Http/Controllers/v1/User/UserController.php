73a3ecbdcc9dceae8a7197a36396e2f5954627f5
alizeinodin <alizeinodin79@gmail.com>
2023-07-28 17:48:53 +0330

    update method add for UserController.php

--- src/app/Http/Controllers/v1/User/UserController.php
+++ src/app/Http/Controllers/v1/User/UserController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\v1\User;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\User\UpdateRequest;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Support\Facades\Auth;
 use Symfony\Component\HttpFoundation\Response;
@@ -13,5 +14,27 @@ class UserController extends Controller
     {
         return response()
             ->json(Auth::user(), Response::HTTP_OK);
+    }
+
+    public function update(UpdateRequest $request): JsonResponse
+    {
+        $validatedData = $request->validated();
+
+        $user = $request->user();
+
+        $user->update([
+            'username' => $validatedData['username'] ?? $user->username,
+            'firstname' => $validatedData['firstname'] ?? $user->firstname,
+            'lastname' => $validatedData['lastname'] ?? $user->lastname,
+            'email' => $validatedData['email'] ?? $user->email,
+            'bio' => $validatedData['bio'] ?? $user->bio
+        ]);
+
+        $response = [
+            'message' => __('اطلاعات با موفقیت بروزرسانی شدند')
+        ];
+
+        return \response()
+            ->json($response, Response::HTTP_ACCEPTED);
     }
 }
